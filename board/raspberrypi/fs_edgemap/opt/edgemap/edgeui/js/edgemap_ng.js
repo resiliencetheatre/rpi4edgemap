c313a273ea84353aa84229bf7a50f3f6d49562be
info <info@resilience-theatre.com>
2025-04-16 06:43:03 +0200

    * Added python bindings to gpsd (BR2_PACKAGE_GPSD_PYTHON)

--- board/raspberrypi/fs_edgemap/opt/edgemap/edgeui/js/edgemap_ng.js
+++ board/raspberrypi/fs_edgemap/opt/edgemap/edgeui/js/edgemap_ng.js
@@ -534,23 +534,15 @@ function submitImage() {
             
             // Create image marker
             createImageMarker("myself",submit_form_latitude, submit_form_longitude, payload );
-            // TODO: can we share this over mirror socket?
-            var subLat = submit_form_latitude.substr(0,10);
-            var subLon = submit_form_longitude.substr(0,10);
-            var myCallSign=getElementItem('#myCallSign').value;
-            var imgMsg = myCallSign + `|imageMarker|[`+subLat+`,`+subLon+`]|`+ payload +  "\n";
             
+            // Demo with mirror socket, share image over to other peers connected to this instance
             if ( mirrorSocketConnected ) {
+                var subLat = submit_form_latitude.substr(0,10);
+                var subLon = submit_form_longitude.substr(0,10);
+                var myCallSign=getElementItem('#myCallSign').value;
+                var imgMsg = myCallSign + `|imageMarker|[`+subLat+`,`+subLon+`]|`+ payload +  "\n";
                 mirrorSocket.send(imgMsg);
             }
-            
-            
-            
-            
-            
-            
-            
-            
             
             var modal = document.getElementById('myModal');
             var images = document.getElementsByClassName('myImages');
